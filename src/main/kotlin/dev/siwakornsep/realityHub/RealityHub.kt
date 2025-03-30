51fa27116ace71018a007e5607a349c8784efa19
Siwakorn_Sep <siwakorntide@gmail.com>
2025-03-30 18:46:43 +0700

    Initial commit

---
+++ src/main/kotlin/dev/siwakornsep/realityHub/RealityHub.kt
@@ -0,0 +1,14 @@
+package dev.siwakornsep.realityHub
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RealityHub : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
